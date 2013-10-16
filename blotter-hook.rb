56d962b16a946eb7824bf86c11ca47f751c58481
Trevor Bedford <trvrb@trevorbedford.com>
2013-10-15 22:58:44 -0700

    Update submodule pull.

--- blotter-hook.rb
+++ blotter-hook.rb
@@ -10,8 +10,32 @@ def update
 	Dir.chdir("blotter")
 	
 	# git pull
-	`git pull --recurse-submodules`
+	`git pull`
+	`git submodule foreach git pull`	# `git pull origin --recurse-submodules` is better, but requires git 1.7.3
+	`git submodule update`
+	
+	# climb back up to parent dir
+	Dir.chdir()
 
 end
 
+# build with jekyll
+def build
+
+	# drop into blotter dir
+	Dir.chdir("blotter")
+	
+	# run jekyll
+	`jekyll build`
+
+end
+
+# deploy to s3
+def deploy
+
+end
+
+# run script
 update
+build
+deploy
